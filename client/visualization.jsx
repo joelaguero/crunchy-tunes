b00a8d1939509152f07f533c14b0f304ea64455c
Joel Aguero <joelmaguero@gmail.com>
2016-04-20 15:12:48 -0700

    Revert "Refactor into class"
    This reverts commit 71416fdb3189cae34670868fcc5b8e57bee189b2.

--- client/visualization.jsx
+++ client/visualization.jsx
@@ -2,17 +2,9 @@ import React from 'react';
 
 const seedData = [133, 100, 30, 99, 153, 170, 249, 253, 75, 116, 109, 203];
 
-class Visualization extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      audioData: seedData,
-    };
-  }
-
-  render() {
-    return (<div className="visualization">{ seedData }</div>);
-  }
-}
+const Visualization = () => {
+  console.log('inside visualization');
+  return (<div className="visualization">{ seedData }</div>);
+};
 
 export default Visualization;
