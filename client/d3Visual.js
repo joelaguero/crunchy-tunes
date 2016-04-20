1dd482edcc86a1d2fbcc91abd66e6e08c97484c0
Joel Aguero <joelmaguero@gmail.com>
2016-04-20 16:41:04 -0700

    Render some circles for seedData

--- client/d3Visual.js
+++ client/d3Visual.js
@@ -2,16 +2,23 @@ import d3 from 'd3';
 
 const d3Visual = {};
 
-d3Visual.create = function create(el, props) {
+d3Visual.create = function create(el, props, audioData) {
   const svg = d3.select(el).append('svg')
-    .attr('class', 'd3')
+    .attr('class', 'visualization-canvas')
     .attr('width', props.width)
     .attr('height', props.height);
 
-  svg.append('g')
-    .attr('class', 'd3-points');
+  const circles = svg.selectAll('circle')
+    .data(audioData)
+    .enter()
+    .append('circle')
+    .attr('r', (d) => (d / 2))
+    .attr('cx', (d) => (d))
+    .attr('cy', (d) => (d));
 
-  // this.update(el, state);
+  circles
+    .style('fill', () => (`#${Math.floor(Math.random() * 16777215).toString(16)}`))
+    .style('opacity', '.5');
 };
 
 export default d3Visual;
--- client/visualization.jsx
+++ client/visualization.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import d3Visual from './d3Visual';
 
 // this is the component that contains the visualization. the visualization is rendered
-// by passing state to a d3 object that handles all d3 rendering. the d3 object is
+// by passing data to a d3 object that handles all d3 rendering. the d3 object is
 // designed to be stateless, meaning it doesn't hold on to any state-related data that
 // gets passed to it.
 class Visualization extends React.Component {
@@ -12,7 +12,7 @@ class Visualization extends React.Component {
     d3Visual.create(el, {
       width: '100%',
       height: '500px',
-    });
+    }, this.props.audioData);
   }
 
   render() {
