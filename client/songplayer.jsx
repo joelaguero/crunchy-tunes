f29b65ce0241b0afb239387d7378e32a77b5656a
Reina Mei <reinameic@gmail.com>
2016-04-19 23:59:07 -0700

    Use audio tag instead of iframe for soundcloud

--- client/songplayer.jsx
+++ client/songplayer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import keys from '../config/keys.js';
 
 const SongPlayer = ({ track }) => {
   let embed;
@@ -8,7 +9,7 @@ const SongPlayer = ({ track }) => {
       embed = <iframe src={`https://embed.spotify.com/?uri=spotify%3Atrack%3A${track.contentId}`} width="500" height="80" frameBorder="0" allowTransparency="true"></iframe>;
       break;
     case 'SoundCloud':
-      embed = <iframe width="500" height="80" scrolling="no" frameBorder="no" src={`https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${track.contentId}&color=orange_white`}></iframe>;
+      embed = <audio controls src={`https://api.soundcloud.com/tracks/${track.contentId}/stream?client_id=${keys.soundCloud}`}></audio> 
       break;
     case 'YouTube':
       embed = <audio controls src={`http://www.youtubeinmp3.com/fetch/?video=http://www.youtube.com/watch?v=${track.contentId}`}></audio>
