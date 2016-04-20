a362493f8065f54a324e7c4f8689182c385c5e6a
Andrew H <aho338@gmail.com>
2016-04-20 16:02:27 -0700

    Use WebAudio API to grab audio data from SoundCloud tracks

--- client/visualizer.js
+++ client/visualizer.js
@@ -13,7 +13,8 @@ var sampleDOMElement = function(DOMElementID) {
   this.analyser.fftSize = 256;
   this.source.connect(this.analyser);
   this.analyser.connect(audioCtx.destination);
-  setInterval(function() { this.sampleAudioStream(analyser, streamData); }, 10);
+  var context = this;
+  setInterval(function() { context.sampleAudioStream(context.analyser, streamData);}, 10);
 }
 
 sampleDOMElement.prototype.sampleAudioStream = function(analyser, streamData) {
@@ -25,36 +26,9 @@ sampleDOMElement.prototype.sampleAudioStream = function(analyser, streamData) {
       // $('#band-' + i).css('background-color', 'hsl('+ (325 - (streamData[i] / (256) * 360)) % 360+', 100%, 50%)');
       volume += streamData[i];
     }
-    return volume;
-    // console.log(streamData);
+    console.log(streamData);
     // console.log(volume);
+    return volume;
 };
-
-
-
-
-// var sampleAudioStream = function(analyser, streamData) {
-//     analyser.getByteFrequencyData(streamData);
-//     // calculate an overall volume value
-//     var volume = 0;
-//     for (var i = 0; i < streamData.length; i++) {
-//       // $('#band-' + i).css('height', streamData[i] / 256 * 100 + 'px');
-//       // $('#band-' + i).css('background-color', 'hsl('+ (325 - (streamData[i] / (256) * 360)) % 360+', 100%, 50%)');
-//       volume += streamData[i];
-//     }
-//     return volume;
-//     // console.log(streamData);
-//     // console.log(volume);
-// };
-
-// var audioCtx = new (window.AudioContext || window.webkitAudioContext);
-// var player = document.getElementById('player');
-// var source = audioCtx.createMediaElementSource(player);
-// var analyser = audioCtx.createAnalyser();
-// analyser.fftSize = 256;
-// source.connect(analyser);
-// analyser.connect(audioCtx.destination);
-//
-// setInterval(function() { sampleAudioStream(analyser, streamData); }, 10);
 
 export default sampleDOMElement;
