6dc05db8824a1a1c2733778e3b17b7eb46a44c38
Reina Mei <reinameic@gmail.com>
2016-04-20 17:04:50 -0700

    Implement song model

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,7 @@
 import Sequelize from 'sequelize';
 import password from '../config/mysqlsetup.js'
 
-var db = new Sequelize('crunchy', 'root', password, {
+const db = new Sequelize('crunchy', 'root', password, {
   define: {timestamps: false}
 });
 
---
+++ server/song/songModel.js
@@ -0,0 +1,33 @@
+import Sequelize from 'sequelize';
+import db from '../db.js';
+
+const Song = db.define('Song',
+  {
+    id: {
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV1,
+      primaryKey: true,
+    },
+    contentId: {
+      type: Sequelize.STRING,
+      field: 'content_id',
+    },
+    imagePath: {
+      type: Sequelize.STRING,
+      filed: 'image_path',
+    }
+    creator: {
+      type: Sequelize.STRING,
+    },
+    songTitle: {
+      type: Sequelize.STRING,
+      field: 'song_title',
+    },
+    apiSource: {
+      type: Sequelize.STRING,
+      field: 'api_source',
+    },
+  }
+);
+
+export default Song;
