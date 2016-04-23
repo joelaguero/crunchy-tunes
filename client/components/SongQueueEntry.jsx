30a414dd85367b0d8adcb2dc9358bf241a00b7f6
Reina Mei <reinameic@gmail.com>
2016-04-22 20:36:01 -0700

    Improve queue logic

--- client/components/SongQueueEntry.jsx
+++ client/components/SongQueueEntry.jsx
@@ -4,7 +4,7 @@ const SongQueueEntry = (props) => (
   <div>
     <p>{props.song.songTitle}</p>
     <p>{props.song.creator}</p>
-    <button onClick={() => {props.handlePlay(props.song);}}>Play</button>
+    <button onClick={() => {props.handlePlay(props.song); props.handleRemoveFromQueue(props.song);}}>Play Now</button>
     <button onClick={() => {props.handleAddToSaved(props.song);}}>Save</button>
     <button onClick={() => {props.handleRemoveFromQueue(props.song);}}>Remove</button>
     <hr></hr>
