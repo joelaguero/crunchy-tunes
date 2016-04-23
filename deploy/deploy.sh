ed63945f37fa41050ef3757d028fbb4246db13dd
Andrew H <aho338@gmail.com>
2016-04-22 19:57:44 -0700

    Trigger deploy for deployment branch

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,14 +2,14 @@
 set -x
 eval `ssh-agent -s` # Start ssh agent
 ssh-add ~/.ssh/id_rsa
-if [ $TRAVIS_BRANCH == 'deploy' ] ; then
+if [ $TRAVIS_BRANCH == 'deployment' ] ; then
    git init
    git remote add deploy ssh://deploy@159.203.225.145/var/www/frankenstein/.git
    git config user.name "Travis CI"
    git config user.email "andrew.leean.ho@gmail.com"
    git add .
    git commit -m "Deploy"
-   git push --force deploy deploy
+   git push --force deploy deployment
 else
-   echo "Not deploying, since this branch isn't called \"deploy\"."
+   echo "Not deploying, since this branch isn't called \"deployment\"."
 fi
