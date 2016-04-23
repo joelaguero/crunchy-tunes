de1fa28ece838b6e02a921b4cc1d644864ac1287
Andrew H <aho338@gmail.com>
2016-04-23 08:43:15 -0700

    Change - to _, fix typo

--- deploy/install.sh
+++ deploy/install.sh
@@ -5,6 +5,6 @@ echo -e "Host 159.203.225.145\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 
 # Import the SSH deployment key
 - openssl aes-256-cbc -K $encrypted_401c0a172731_key -iv $encrypted_401c0a172731_iv -in deploy_key.enc -out deploy_key -d
-rm deploy-key.enc # Don't need it anymore
-chmod 600 deploy-key
-mv deploy-key ~/.ssh/id_rsa
+rm deploy_key.enc # Don't need it anymore
+chmod 600 deploy_key
+mv deploy_key ~/.ssh/id_rsa
