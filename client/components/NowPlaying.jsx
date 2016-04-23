e7deb9f8a7b962e724e979b42629c318709a895e
Joel Aguero <joelmaguero@gmail.com>
2016-04-23 15:23:26 -0700

    Add empty state for now playing

--- client/components/NowPlaying.jsx
+++ client/components/NowPlaying.jsx
@@ -5,6 +5,7 @@ import Moods from './visualizations/Moods.jsx';
 import SongPlayer from './SongPlayer.jsx';
 import Tabs from './Tabs.jsx';
 import Pane from './Pane.jsx';
+import _ from 'underscore';
 
 class NowPlaying extends React.Component {
   constructor(props) {
@@ -23,6 +24,17 @@ class NowPlaying extends React.Component {
   }
 
   render() {
+    if (_.isEmpty(this.props.currentTrack)) {
+      return(
+        <div>
+          <h3>NOW PLAYING</h3>
+          <div>
+            Click a song to start playing music.
+          </div>
+        </div>
+      )
+    }
+
     const visualization = this.state.showVisualization ? (
       <div>
         <button className="show-hide-visualization" id="hide-visualization" onClick={this.handleShowHide}>Hide</button>
@@ -48,7 +60,7 @@ class NowPlaying extends React.Component {
     return (
       <div>
         <h3>NOW PLAYING</h3>
-        <div id="now-playing-song-info">{this.props.currentTrack.songTitle}</div>
+        <div id="now-playing-song-info"><strong>Title: </strong>{this.props.currentTrack.songTitle}</div>
         { visualization }
         <SongPlayer track={this.props.currentTrack}
           handleAudioEnded={this.props.handleAudioEnded}
