f55869f91b2eca044ed8da6e20e01ccb84c8835a
Reina Mei <reinameic@gmail.com>
2016-04-23 12:09:13 -0700

    Only show saved songs when logged in

--- client/components/SavedSongContainer.jsx
+++ client/components/SavedSongContainer.jsx
@@ -1,18 +1,32 @@
 import React from 'react';
 import SavedSongEntry from './SavedSongEntry.jsx';
 
-const SavedSongContainer = (props) => (
-  <div>
-    <h3>MY SONGS</h3>
-    {props.savedSongs.map(song =>
+const SavedSongContainer = (props) => {
+
+  var show = props.user ? 
+    (
+      <div>
+      <h3>MY SAVED SONGS</h3>
+      {props.savedSongs.map(song =>
         <SavedSongEntry
+          user={props.user}
           key={song.contentId}
           handlePlay={props.handlePlay}
-          handleAddToQueue={props.handleAddToQueue}
           handleRemoveFromSaved={props.handleRemoveFromSaved}
+          handleAddToQueue={props.handleAddToQueue}
           song={song}
-        />)}
-  </div>
-);
+        />
+        )
+      }
+      </div>
+    ) : null;
+
+  return (
+    <div>
+      {show}
+    </div>
+  );
+
+};
 
 export default SavedSongContainer;
