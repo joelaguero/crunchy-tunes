a1e2dc950bc5199fe650aaf9213ca49acfa57ddf
Andrew H <aho338@gmail.com>
2016-04-23 11:41:34 -0700

    Remove _ from install.sh

--- deploy/install.sh
+++ deploy/install.sh
@@ -8,5 +8,4 @@ echo -e "Host 159.203.225.145\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 openssl aes-256-cbc -K $encrypted_401c0a172731_key -iv $encrypted_401c0a172731_iv -in deploy-key.enc -out deploy-key -d
 rm deploy-key.enc # Don't need it anymore
 chmod 600 deploy-key
-
 mv deploy-key ~/.ssh/id_rsa
