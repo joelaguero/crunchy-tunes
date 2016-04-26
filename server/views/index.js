7b22d7b0dff39d6cd5a5031f04086bf6926747d7
Joel Aguero <joelmaguero@gmail.com>
2016-04-26 10:40:22 -0700

    Add meta tags

--- server/views/index.js
+++ server/views/index.js
@@ -3,8 +3,28 @@ module.exports = function renderFullPage(initialState) {
   <!DOCTYPE html>
   <html>
     <head>
-      <title>FRANKIE CRICKET</title>
       <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+
+      <title>FRANKIE CRICKET</title>
+      <meta name="description" content="Tjio." />
+
+      <!-- Schema.org markup for Google+ -->
+      <meta itemprop="name" content="Frankie Cricket">
+      <meta itemprop="description" content="Cross-platform music player for SoundCloud and YouTube.">
+      <meta itemprop="image" content="http://i.imgur.com/DB7mtAG.png">
+
+      <!-- Twitter Card data -->
+      <meta name="twitter:card" content="product">
+      <meta name="twitter:title" content="Frankie Cricket">
+      <meta name="twitter:description" content="Cross-platform music player for SoundCloud and YouTube.">
+      <meta name="twitter:image" content="http://i.imgur.com/DB7mtAG.png">
+
+      <!-- Open Graph data -->
+      <meta property="og:title" content="Cross-platform music player for SoundCloud and YouTube. " />
+      <meta property="og:url" content="http://www.frankiecricket.com/" />
+      <meta property="og:image" content="http://i.imgur.com/DB7mtAG.png" />
+      <meta property="og:description" content="More music, more fun. Build playlists using SoundCloud AND YouTube." />
+      <meta property="og:site_name" content="Frankie Cricket" />
 
       <link rel="stylesheet" href="./styles/styles.css" charset="utf-8">
       <link rel="stylesheet" href="./styles/simple-grid.css" charset="utf-8">
