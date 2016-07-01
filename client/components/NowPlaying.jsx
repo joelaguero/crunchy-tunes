d738f6dd9ff34d00e998f017f3b77a8b606b601d
Joel Aguero <joelaguero@users.noreply.github.com>
2016-07-01 09:12:16 -0700

    Add comments

--- client/components/NowPlaying.jsx
+++ client/components/NowPlaying.jsx
@@ -7,6 +7,12 @@ import Tabs from './Tabs.jsx';
 import Pane from './Pane.jsx';
 import _ from 'underscore';
 
+/*
+ * This class displays the Now Playing component in the left rail. It 
+ * is a parent of the visualization components and the show/hide
+ * toggle. The show/hide toggle is stored as a boolean in this component's
+ * state.
+ */
 class NowPlaying extends React.Component {
   constructor(props) {
     super(props);
@@ -25,6 +31,8 @@ class NowPlaying extends React.Component {
 
   render() {
     if (_.isEmpty(this.props.currentTrack)) {
+      
+      // render an empty state if there is no track currentlying playing
       return(
         <div>
           <h3>NOW PLAYING</h3>
@@ -38,7 +46,9 @@ class NowPlaying extends React.Component {
         </div>
       )
     }
-
+    
+    // determine the correct JSX to render based on the state of the showVisualization
+    // boolean in state.
     const visualization = this.state.showVisualization ? (
       <div>
         <button className="show-hide-visualization" id="hide-visualization" onClick={this.handleShowHide}>Hide</button>
@@ -62,6 +72,8 @@ class NowPlaying extends React.Component {
       </div>
     );
     if (this.props.currentTrack.apiSource === 'YouTube') {
+      // hide the visualization if the source is YouTube, because of the cross-origin issue that
+      // prevents us from using the audio stream with the WebAudio API
       return (
         <div>
           <h3>NOW PLAYING</h3>
@@ -72,6 +84,8 @@ class NowPlaying extends React.Component {
         </div>
       );
     }
+    
+    // otherwise the source is Soundcloud, so render the visualization.
     return (
       <div>
         <h3>NOW PLAYING</h3>
